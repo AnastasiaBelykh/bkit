ef208bc3ff9a0bbfaaed8f9f52e97e25a839c121
AnastasiaBelykh <34924089+AnastasiaBelykh@users.noreply.github.com>
2017-12-29 17:24:57 +0300

    Add files via upload

--- Program.cs
+++ Program.cs
@@ -2,82 +2,85 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
-using System.Threading.Tasks;
 
-namespace Lab2
+
+namespace Lab6_1
 {
-    abstract class Geometric_figure
+    class Program
     {
-        public string Type { get; set; }
-        public abstract double Area();
-        public override string ToString() //переопределение Object
-        {
-            return " Площадь " + this.Type + " " + this.Area().ToString();
-        }
-    }
-    interface IPrint
-    {
-        void Print();
-    }
-    class Rectangle : Geometric_figure, IPrint  
-    {
-        public double Height { get; set; } 
-        public double Width { get; set; } 
-        public Rectangle(double a, double b) //конструктор 
-        {
-            this.Height = a;
-            this.Width = b;
-            this.Type = "Прямоугольника";
-        }
-        public override double Area()
+        public delegate float roots(float p1, int p2);
 
+        static roots discr = (float p1, int p2) =>
         {
-            double Result = this.Width * this.Height;
-            return Result;
+            return p1 * p1 - 4 * p2;
+
+        };
+        static Func<float, int, float> discr1 = (float p1, int p2) =>
+        {
+            return p1 * p1 - 4 * p2;
+
+        };
+        static float Findroot(float p1, int p2)
+        {
+            return p1 / (2 * p2);
         }
 
-        public void Print()
+
+
+        static void equation(int a, int b, int c, roots discr, roots root)
         {
-            Console.WriteLine(this.ToString());
+            if (discr(b, a * c) > 0)
+            {
+                Console.WriteLine("1 корень: " + root(-b + discr(b, a * c), a));
+                Console.WriteLine("2 корень: " + root(-b - discr(b, a * c), a));
+            }
+            else
+                if (discr(b, a * c) == 0)
+                Console.WriteLine("Корень: " + root(-b, a));
+
+            else
+                if (discr(b, a * c) < 0)
+                Console.WriteLine("Нет корней");
         }
-    }
-    class Square : Rectangle, IPrint
-    {
-        public Square(double size) : base(size, size)
+
+        static void equation1(int a, int b, int c, Func<float, int, float> discr, Func<float, int, float> root)
         {
-            this.Type = "Квадрата";
+            if (discr(b, a * c) > 0)
+            {
+                Console.WriteLine("1 корень: " + root(-b + discr(b, a * c), a));
+                Console.WriteLine("2 корень: " + root(-b - discr(b, a * c), a));
+            }
+            else
+                if (discr(b, a * c) == 0)
+                Console.WriteLine("Корень: " + root(-b, a));
+
+            else
+                if (discr(b, a * c) < 0)
+                Console.WriteLine("Нет корней");
         }
-    }
-    class Circle : Geometric_figure, IPrint
-    {
-        public double Radius { get; set; }
-        public Circle(double r)
-        {
-            this.Radius = r;
-            this.Type = "Круга";
-        }
-        public override double Area()
-        {
-            double Result = Math.PI * this.Radius * this.Radius;
-            return Result;
-        }
-        public void Print()
-        {
-            Console.WriteLine(this.ToString());
-        }
-    }
-    class programm
-    {
+
         static void Main(string[] args)
         {
-            Rectangle rect = new Rectangle(5, 4);
-            Square square = new Square(5);
-            Circle circle = new Circle(5);
+            int a, b, c;
+            Console.WriteLine("Введите a, b, c");
+            Console.Write("a: ");
+            a = Int32.Parse(Console.ReadLine());
+            Console.Write("b: ");
+            b = Int32.Parse(Console.ReadLine());
+            Console.Write("c: ");
+            c = Int32.Parse(Console.ReadLine());
 
-            rect.Print();
-            square.Print();
-            circle.Print();
-            Console.ReadLine();
+            Console.WriteLine();
+            Console.WriteLine("Поиск корней с использованием делегатов и выражения лямбда:");
+            equation(a, b, c, discr, Findroot);
+
+            Console.WriteLine();
+            Console.WriteLine("Нахождение корней с помощью функции:");
+            equation1(a, b, c, discr1, Findroot);
+
+            Console.WriteLine();
+            Console.WriteLine("Нажмите любую клавишу для продолжения");
+            Console.Read();
         }
     }
 }
